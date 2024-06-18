b4e5a87c733d84e8e686d9163c2bdc658ec2eefe
luizfelipew <luizfelipewendt@gmail.com>
2024-06-18 19:18:23 -0300

    enviando email com SES

--- projeto-2/handler.py
+++ projeto-2/handler.py
@@ -3,6 +3,7 @@ import boto3
 
 sessao = boto3.Session(profile_name='automacao-curso', region_name='us-east-1')
 cliente_s3 = sessao.client('s3')
+cliente_ses = sessao.client('ses')
 
 
 def envio_email(event, context):
@@ -12,11 +13,14 @@ def envio_email(event, context):
     )
 
     dados = resultado['Body'].readlines()
+    dados = dados[1:]
     for dado in dados:
         linha = dado.decode('utf-8')
-        linha = linha.strip() # serve para tirar os \r\n etc
+        linha = linha.strip()  # serve para tirar os \r\n etc
         usuario = linha.split(',')
         print(usuario)
+        enviar_email(usuario[0], usuario[1])
+        break
 
     body = {
         "message": "Go Serverless v3.0! Your function executed successfully!",
@@ -24,5 +28,31 @@ def envio_email(event, context):
     }
 
     return {"statusCode": 200, "body": json.dumps(body)}
+
+
+def enviar_email(nome, email):
+    cliente_ses.send_email(
+        Source='confusaosk8@gmail.com',
+        Destination={
+            'BccAddresses': [],
+            'CcAddresses': [],
+            'ToAddresses': [
+                email
+            ]
+        },
+        Message={
+            'Subject': {
+                'Charset': 'utf-8',
+                'Data': 'Mensagem do Projeto 2'
+            },
+            'Body': {
+                'Html': {
+                    'Charset': 'utf-8',
+                    'Data': f'Olá, mensagem enviada via serverless sr/sra: {nome}. Boas vindas Serverless.'
+                }
+            }
+        }
+    )
+
 
 envio_email({}, {})
